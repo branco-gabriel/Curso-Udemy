f78cd65c558ef32e1cfbc2cc0493dd99fbe73123
Gabriel <brancogabriel2@gmail.com>
2023-05-05 08:10:36 -0300

    some tweaks to the models

--- recipes/models.py
+++ recipes/models.py
@@ -1,9 +1,12 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
@@ -22,7 +25,11 @@ class Recipe(models.Model):
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True
-        )
+    )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
-        )
+    )
+
+
+    def __str__(self):
+        return self.title
