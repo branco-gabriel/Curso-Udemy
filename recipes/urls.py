b9d682854f5f74e697c32742788cedf1c18d78d5
Gabriel <brancogabriel2@gmail.com>
2023-05-10 08:18:36 -0300

    Tweaks to models  and url.py Inserted new template: category.html

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('recipe/category/<int:category_id>/', views.category, name='category'),
     path('recipe/<int:id>/', views.recipe, name='recipe'),
 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from recipes.models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe for i in range(10)],
+        'recipes': recipes,
     })
 
 
@@ -13,3 +15,10 @@ def recipe(request, id):
         'recipe': make_recipe(),
         'is_detail_page': True,
     })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        })
